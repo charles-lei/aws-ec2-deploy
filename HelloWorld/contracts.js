1877901c016ad62490752663f15d45df84ef5af4
leili <leili@meowtechnology.com>
2018-04-14 10:14:05 +0800

    fix gas prcie

--- HelloWorld/contracts.js
+++ HelloWorld/contracts.js
@@ -282,7 +282,8 @@ const contractAbi = [
 const contract = new Contract(rpcAddress, contractAddress, contractAbi)
 contract.send('transfer', {
     methodArgs: [address, Number(send_nodes_count)*100000000],
-    senderAddress: 'QV4PR5nre19w2NNVXZLCBPJYDosob92fhq'
+    senderAddress: 'QV4PR5nre19w2NNVXZLCBPJYDosob92fhq',
+    gasPrice: 0.0000002
     //testnet: qLjv8pnuuccSHAmAHfB1s8uT5Ru7xNnK9x
     //mainnet: QV4PR5nre19w2NNVXZLCBPJYDosob92fhq
   }).catch(function(error) {
