88aad58970a1334bd1956589617392065416a58c
leili <leili@meowtechnology.com>
2018-04-26 11:08:08 +0800

    add webfont and layout ajustment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def url
     Rails.env == 'production' ? 'http://node.meowtechnology.com': 'http://192.168.31.184:3000'
   end
+
+  def title
+    if params[:controller] == 'welcome'
+      'Node签到'
+    elsif params[:controller] == 'giveaway'
+      'Node抽奖'
+    end
+  end
 end
