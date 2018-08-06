174b6be9842d28886a0d4433c30a00d017510cc1
leili <leili@meowtechnology.com>
2018-08-06 09:21:07 +0800

    contract update

--- ERC20.sol
+++ ERC20.sol
@@ -2,21 +2,21 @@ pragma solidity ^0.4.18;
 
 contract SafeMath {
 
-    function SafeMath() {
+    constructor() public {
     }
 
-    function safeAdd(uint256 _x, uint256 _y) internal returns (uint256) {
+    function safeAdd(uint256 _x, uint256 _y) internal pure returns (uint256) {
         uint256 z = _x + _y;
         assert(z >= _x);
         return z;
     }
 
-    function safeSub(uint256 _x, uint256 _y) internal returns (uint256) {
+    function safeSub(uint256 _x, uint256 _y) internal pure returns (uint256) {
         assert(_x >= _y);
         return _x - _y;
     }
 
-    function safeMul(uint256 _x, uint256 _y) internal returns (uint256) {
+    function safeMul(uint256 _x, uint256 _y) internal pure returns (uint256) {
         uint256 z = _x * _y;
         assert(_x == 0 || z / _x == _y);
         return z;
